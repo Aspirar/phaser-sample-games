84efbefc5bc663ac6620b90345786208de43727b
Lakshya Gupta <theultimateaim@gmail.com>
2019-11-11 01:22:20 +0530

    Complete stick hero game

--- stick-hero/game.js
+++ stick-hero/game.js
@@ -112,6 +112,7 @@ class PlayGame extends Phaser.Scene {
 
 	grow() {
 		if (this.gameMode !== WAITING) return
+		this.gameMode = GROWING
 		this.growTween = this.tweens.add({
 			targets: [this.pole],
 			displayHeight: gameOptions.platformGapRange[1]
@@ -120,11 +121,141 @@ class PlayGame extends Phaser.Scene {
 		})
 	}
 
+	fallAndDie() {
+		this.tweens.add({
+			targets: [this.player],
+			y: game.config.height + this.player,
+			duration: gameOptions.fallTime,
+			ease: 'Cubic.easeIn',
+			callbackScope: this,
+			onComplete() {
+				this.cameras.main.shake(800, 0.01)
+				this.time.addEvent({
+					delay: 2000,
+					callbackScope: this,
+					callback() {
+						this.scene.start('PlayGame')
+					},
+				})
+			},
+		})
+	}
+
+	platformTooLong() {
+		this.tweens.add({
+			targets: [this.player],
+			x: this.pole.x
+				+ this.pole.displayHeight
+				+ this.player.displayWidth,
+			duration: gameOptions.walkTime,
+			callbackScope: this,
+			onComplete() {
+				this.fallAndDie()
+			},
+		})
+	}
+
+	platformTooShort() {
+		this.tweens.add({
+			targets: [this.pole],
+			angle: 90,
+			duration: gameOptions.rotateTime,
+			ease: 'Cubic.easeIn',
+			callbackScope: this,
+			onComplete() {
+				this.tweens.add({
+					targets: [this.player],
+					x: this.pole.x + this.pole.displayHeight,
+					duration: gameOptions.walkTime,
+					callbackScope: this,
+					onComplete() {
+						this.tweens.add({
+							targets: [this.pole],
+							angle: 180,
+							duration: gameOptions.rotateTime,
+							ease: 'Cubic.easeIn',
+						})
+						this.fallAndDie()
+					},
+				})
+			},
+		})
+	}
+
+	prepareNextMove() {
+		this.platforms[this.mainPlatform].x = game.config.width
+		this.mainPlatform = 1 - this.mainPlatform
+		this.tweenPlatform()
+		this.pole.angle = 0
+		this.pole.x = this.platforms[this.mainPlatform].displayWidth
+		this.pole.displayHeight = gameOptions.poleWidth
+	}
+
+	stop() {
+		if (this.gameMode !== GROWING) return
+		this.gameMode = IDLE
+		this.growTween.stop()
+
+		if (this.pole.displayHeight > (
+			this.platforms[1 - this.mainPlatform].x - this.pole.x
+		)) {
+			this.tweens.add({
+				targets: [this.pole],
+				angle: 90,
+				duration: gameOptions.rotateTime,
+				ease: 'Bounce.easeOut',
+				callbackScope: this,
+				onComplete() {
+					if (!(this.pole.displayHeight < (
+						this.platforms[1 - this.mainPlatform].x
+							+ this.platforms[1 - this.mainPlatform].displayWidth
+							- this.pole.x
+					))) {
+						this.platformTooLong()
+						return
+					}
+
+					this.tweens.add({
+						targets: [this.player],
+						x: this.platforms[1 - this.mainPlatform].x
+							+ this.platforms[1 - this.mainPlatform].displayWidth
+							- this.pole.displayWidth,
+						duration: gameOptions.walkTime,
+						callbackScope: this,
+						onComplete() {
+							this.tweens.add({
+								targets: [
+									this.player,
+									this.pole,
+									this.platforms[1 - this.mainPlatform],
+									this.platforms[this.mainPlatform],
+								],
+								props: {
+									x: {
+										value: `-=${this.platforms[1 - this.mainPlatform].x}`,
+									},
+								},
+								duration: gameOptions.scrollTime,
+								callbackScope: this,
+								onComplete() {
+									this.prepareNextMove()
+								},
+							})
+						},
+					})
+				},
+			})
+		} else {
+			this.platformTooShort()
+		}
+	}
+
 	create() {
 		this.addPlatforms()
 		this.addPlayer()
 		this.addPole()
 		this.input.on('pointerdown', this.grow, this)
+		this.input.on('pointerup', this.stop, this)
 	}
 }
 
